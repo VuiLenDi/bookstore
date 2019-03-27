a77fa7a0307ca11c1d58cd1e9a2c727feea2f59f
VuiLenDi <vuilendi@gmail.com>
2019-03-28 02:22:49 +0700

    Test case in back-end

---
+++ back-end/tests/routes/router.test.js
@@ -0,0 +1,43 @@
+const request = require('supertest');
+const app = require('../../app');
+const http = require('http');
+
+const port = 3000;
+let server = http.createServer(app);
+
+beforeAll((done) => {
+    console.log('khoi tao server');
+    server.listen({ port }, done)
+});
+
+afterAll(() => {
+    console.log('vao day dong app');
+    return server.close();
+});
+
+describe('Test the root path', () => {
+    test('It should response the GET method', () => {
+        return request(server).get('/').expect(200);
+    });
+});
+
+describe('Test the /api/books/list path', () => {
+    test('It should response the GET method', () => {
+        return request(server).get('/api/books/list').expect(200);
+    });
+    test('It should return 404 with the POST method', () => {
+        return request(server).post('/api/books/list').expect(404);
+    });
+    test('It should return 404 with the PUT method', () => {
+        return request(server).put('/api/books/list').expect(404);
+    });
+});
+
+describe('Test the /api/books/addBook path', () => {
+    test('It should return 404 with the GET method', () => {
+        return request(server).get('/api/books/addBook').expect(404);
+    });
+    test('It should return 404 with the PUT method', () => {
+        return request(server).put('/api/books/addBook').expect(404);
+    });
+});
