1bf838da89b266824bd65ffa0fef0146e77a31ba
Thuan Nguyen Nhat <thuan.nguyennhat@nashtechglobal.com>
2019-05-17 17:01:32 +0700

    Add sample verify token

--- back-end/services/v1.api.js
+++ back-end/services/v1.api.js
@@ -1,10 +1,24 @@
-const book_api_routes = require('./v1/books.api');
-const auth_api_route = require('./v1/auth.api');
+const jwt = require("jsonwebtoken");
+const secretKey = require("../config").tokenKey;
+const book_api_routes = require("./v1/books.api");
+const auth_api_route = require("./v1/auth.api");
 
-const express = require('express');
+const express = require("express");
 const v1_api_route = express.Router();
 
-v1_api_route.use('/books', book_api_routes);
-v1_api_route.use('/auth', auth_api_route);
+function validateUser(req, res, next) {
+  jwt.verify(req.headers["x-access-token"], secretKey, function(err, decoded) {
+    if (err) {
+      res.json({ status: "error", message: err.message, data: null });
+    } else {
+      // add user id to request
+      req.body.userId = decoded.id;
+      next();
+    }
+  });
+}
+
+v1_api_route.use("/books", validateUser, book_api_routes);
+v1_api_route.use("/auth", auth_api_route);
 
 module.exports = v1_api_route;
