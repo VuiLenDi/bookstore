6780638f169f102e18f4b333e84a44e17d058e0f
Thuan Nguyen Nhat <thuan.nguyennhat@nashtechglobal.com>
2019-05-17 16:36:18 +0700

    Implement Register & Login API

--- back-end/services/v1.api.js
+++ back-end/services/v1.api.js
@@ -1,8 +1,10 @@
 const book_api_routes = require('./v1/books.api');
+const auth_api_route = require('./v1/auth.api');
 
 const express = require('express');
 const v1_api_route = express.Router();
 
 v1_api_route.use('/books', book_api_routes);
+v1_api_route.use('/auth', auth_api_route);
 
 module.exports = v1_api_route;
